3166fba10ed720652de1f3dcff0070c4f6e6d83c
Joshua George <joshua.vincent.george@gmail.com>
2024-06-27 16:13:22 -0700

    added calcTime function

---
+++ controller/distanceCalculator.js
@@ -0,0 +1,34 @@
+const { Client } = require("@googlemaps/google-maps-services-js");
+require("dotenv").config();
+
+const client = new Client({});
+
+const calcTime = async (homeLocation, eventLocation) => {
+	try {
+		const response = await client.distancematrix({
+			params: {
+				origins: [homeLocation],
+				destinations: [eventLocation],
+				mode: "driving",
+				key: process.env.MAPS_API_KEY,
+			},
+		});
+
+		const result = response.data.rows[0].elements[0];
+		if (result.status === "OK") {
+			const duration = result.duration.text;
+			console.log(`Driving time from home to event: ${duration}`);
+			return duration;
+		} else {
+			console.error("Error calculating driving time:", result.status);
+			return null;
+		}
+	} catch (error) {
+		console.error("Error calculating driving time:", error);
+		return null;
+	}
+};
+
+module.exports = {
+	calcTime,
+};
