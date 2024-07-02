a28a25acc43f195aff964dfa6fdda28c520c7f45
Joshua George <joshua.vincent.george@gmail.com>
2024-07-02 16:41:49 -0700

    updated redirect to front end url

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get(
 	"/google/callback",
 	passport.authenticate("google", { failureRedirect: "/" }),
 	(req, res) => {
-		res.redirect("/");
+		res.redirect("http://localhost:3000/");
 	}
 );
 
