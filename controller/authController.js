649b375ce0df5c49f462880fc8548594221a469c
Joshua George <joshua.vincent.george@gmail.com>
2024-07-02 14:28:11 -0700

    moved ensureAuthenticated

---
+++ controller/authController.js
@@ -0,0 +1,11 @@
+const ensureAuthenticated = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.status(401).json({ error: "User not authenticated" });
+	}
+};
+
+module.exports = {
+	ensureAuthenticated,
+};
