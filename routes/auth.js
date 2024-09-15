1617f5a0e4acf05ba292c5d59a108ae794a4784d
JoshuaVGeorge <joshua.vincent.george@gmail.com>
2024-09-14 18:50:26 -0700

    updated callback uri

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get(
 	"/google/callback",
 	passport.authenticate("google", { failureRedirect: "/" }),
 	(req, res) => {
-		res.redirect("http://localhost:3000/login");
+		res.redirect("http://localhost:3000");
 	}
 );
 
