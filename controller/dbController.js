f03a644ec7bb87380df12b8b3730f7b70c0c1765
JoshuaVGeorge <joshua.vincent.george@gmail.com>
2024-10-04 01:00:14 -0700

    addUser function added

--- controller/dbController.js
+++ controller/dbController.js
@@ -5,3 +5,20 @@ const SUPABASE_URL = process.env.SUPA_URL;
 const SUPABASE_ANON_KEY = process.env.SUPA_KEY;
 
 const supabase = createClient(SUPA_URL, SUPA_KEY);
+
+const addUser = async (id, email) => {
+	const { data, error } = await supabase
+		.from("user_information") // Specify the table name
+		.insert([
+			{ user_id: id, user_email: email }, // Insert user data
+		]);
+
+	if (error) {
+		console.error("Error adding user:", error);
+		return { success: false, error };
+	}
+
+	return { success: true, data };
+};
+
+module.exports = { addUser };
