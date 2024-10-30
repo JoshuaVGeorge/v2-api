0585fa692f35ea24e6cb6f71903c2fafb80fda46
JoshuaVGeorge <joshua.vincent.george@gmail.com>
2024-10-29 20:06:54 -0700

    added env

--- config/supabaseStore.js
+++ config/supabaseStore.js
@@ -1,5 +1,9 @@
 const session = require("express-session");
 const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
+
+const SUPABASE_URL = process.env.SUPA_URL;
+const SUPABASE_ANON_KEY = process.env.SUPA_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
