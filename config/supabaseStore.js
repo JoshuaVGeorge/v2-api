94ed13e5ae638557176feec1b25167e230313aa6
JoshuaVGeorge <joshua.vincent.george@gmail.com>
2024-10-29 19:07:47 -0700

    get method added

--- config/supabaseStore.js
+++ config/supabaseStore.js
@@ -3,6 +3,23 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-class SupabaseStore extends session.Store {}
+class SupabaseStore extends session.Store {
+	async get(sessionId, callback) {
+		try {
+			const { data, error } = await supabase
+				.from("session_information")
+				.select("user_id")
+				.eq("session_id", sessionId)
+				.single();
+
+			if (error) return callback(error, null);
+			if (!data) return callback(null, null);
+
+			callback(null, data.data);
+		} catch (err) {
+			callback(err, null);
+		}
+	}
+}
 
 export default SupabaseStore;
