da7e1f6086f363b469280e071b51e682485026f0
JoshuaVGeorge <joshua.vincent.george@gmail.com>
2024-10-31 22:02:54 -0700

    removed logs

--- config/supabaseStore.js
+++ config/supabaseStore.js
@@ -76,9 +76,9 @@ class SupabaseStore extends session.Store {
 
 	async destroy(sessionId, callback) {
 		try {
-			console.log(
-				`Attempting to destroy session data for sessionId: ${sessionId}`
-			);
+			// console.log(
+			// 	`Attempting to destroy session data for sessionId: ${sessionId}`
+			// );
 			const { error } = await supabase
 				.from("session_information")
 				.delete()
@@ -92,9 +92,9 @@ class SupabaseStore extends session.Store {
 				return callback(error);
 			}
 
-			console.log(
-				`Successfully destroyed session data for sessionId: ${sessionId}`
-			);
+			// console.log(
+			// 	`Successfully destroyed session data for sessionId: ${sessionId}`
+			// );
 			callback(null);
 		} catch (err) {
 			console.error(
